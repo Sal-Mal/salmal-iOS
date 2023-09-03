5d8ebdc38bf4d151041111cbfa30124cb624ccd9
FirstDo <whqtkf12@naver.com>
2023-09-03 14:56:31 +0900

    feat: SMCapsuleTextField에 버튼추가 및 주석추가

--- Projects/UI/Sources/TextField/SMCapsuleTextField.swift
+++ Projects/UI/Sources/TextField/SMCapsuleTextField.swift
@@ -2,6 +2,11 @@ import SwiftUI
 
 public extension SMCapsuleTextField {
   
+  /**
+   color 속성을 변경
+   
+   foreground, background, tinit, placeholder 변경가능
+   */
   func color(_ colors: Self.Colors) -> Self {
     var new = self
     
@@ -19,6 +24,9 @@ public extension SMCapsuleTextField {
     return new
   }
   
+  /**
+    상단에 title 추가
+   */
   func title(
     _ text: String,
     color: Color =  UIAsset.gray2.swiftUIColor,
@@ -31,18 +39,41 @@ public extension SMCapsuleTextField {
     return new
   }
   
+  /**
+    왼쪽에 image 삽입
+   */
   func leftImage(_ name: String) -> Self {
     var new = self
     new.imageName = name
     return new
   }
   
+  /**
+  오른쪽에 button 생성
+   
+   button의 font, color는 각각 .font(), .color() 로 설정된 값을 따름
+   */
+  func rightButton(_ name: String, action: @escaping () -> Void) -> Self {
+    var new = self
+    new.buttonTitle = name
+    new.buttonAction = action
+    return new
+  }
+  
+  /**
+    font를 변경
+   */
   func font(_ font: Font) -> Self {
     var new = self
     new.font = font
     return new
   }
   
+  /**
+   text의 limitLimit를 설정
+   
+   lineLimit > 1 이면 height가 flexible, scrollable 하게 동작
+   */
   func lineLimit(_ num: Int) -> Self {
     var new = self
     new.lineLimit = num
@@ -50,7 +81,9 @@ public extension SMCapsuleTextField {
   }
 }
 
-
+/**
+Capsule 모양의 TextField
+ */
 public struct SMCapsuleTextField: View {
   
   public enum Colors {
@@ -67,6 +100,8 @@ public struct SMCapsuleTextField: View {
   var titleFont: Font = .callout
   
   var imageName: String?
+  var buttonTitle: String?
+  var buttonAction: () -> Void = {}
   var lineLimit: Int = 1
   
   var textColor: Color = UIAsset.white.swiftUIColor
@@ -91,11 +126,28 @@ public struct SMCapsuleTextField: View {
       HStack(spacing: 8) {
         imageView
         
-        if lineLimit >= 2 {
-          flexibleTextField
-        } else {
-          fixedTextField
+        HStack(alignment: .bottom) {
+          textField
+          
+          if let buttonTitle {
+            Spacer()
+            Button(action: buttonAction) {
+              Text(buttonTitle)
+                .foregroundColor(tintColor)
+                .font(font)
+                .padding([.top, .bottom, .trailing], 16)
+                .frame(minHeight: 48)
+            }
+            .opacity(text.isEmpty ? 0 : 1)
+            .animation(.default, value: text.isEmpty)
+          }
         }
+        .background(RoundedRectangle(cornerRadius: 24).fill(bgColor))
+        .overlay(
+          RoundedRectangle(cornerRadius: 24)
+            .strokeBorder(focus ? tintColor : bgColor, lineWidth: 2)
+            .animation(.default, value: focus)
+        )
       }
     }
   }
@@ -121,52 +173,22 @@ public struct SMCapsuleTextField: View {
   }
   
   @ViewBuilder
-  var fixedTextField: some View {
-    TextField(
-      "",
-      text: $text,
-      prompt: Text(placeholder)
-        .foregroundColor(placeholderColor)
-        .font(font)
-    )
-    .focused($focus)
-    .lineLimit(lineLimit)
-    .font(font)
-    .foregroundColor(textColor)
-    .tint(tintColor)
-    .padding()
-    .frame(minHeight: 48)
-    .background(RoundedRectangle(cornerRadius: 24).fill(bgColor))
-    .overlay(
-      RoundedRectangle(cornerRadius: 24)
-        .strokeBorder(focus ? tintColor : bgColor, lineWidth: 2)
-        .animation(.default, value: focus)
-    )
-  }
-  
-  @ViewBuilder
-  var flexibleTextField: some View {
+  var textField: some View {
     TextField(
       "",
       text: $text,
       prompt: Text(placeholder)
         .foregroundColor(placeholderColor)
         .font(font),
-      axis: .vertical
+      axis: lineLimit > 1 ? .vertical : .horizontal
     )
     .focused($focus)
     .lineLimit(lineLimit)
     .font(font)
     .foregroundColor(textColor)
     .tint(tintColor)
-    .padding()
+    .padding([.top, .bottom, .leading], 16)
     .frame(minHeight: 48)
-    .background(RoundedRectangle(cornerRadius: 24).fill(bgColor))
-    .overlay(
-      RoundedRectangle(cornerRadius: 24)
-        .strokeBorder(focus ? tintColor : bgColor, lineWidth: 2)
-        .animation(.default, value: focus)
-    )
   }
 }
 
