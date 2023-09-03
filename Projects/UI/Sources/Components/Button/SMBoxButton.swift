d30fb99c344b62e087d7f7729375df9bb023a49b
devseu <seu0313@kakao.com>
2023-09-03 14:33:24 +0900

    feat: SMBoxButton 구현
    - 버튼 타이틀, 사이즈, 타입, Radius 지정할 수 있도록 구현
    - 버튼 전체 영역에 액션이 전달되도록 구현

---
+++ Projects/UI/Sources/Components/Button/SMBoxButton.swift
@@ -0,0 +1,207 @@
+//
+//  SMBoxButton.swift
+//  UI
+//
+//  Created by 청새우 on 2023/09/03.
+//
+
+import SwiftUI
+
+struct SMBoxButton: View {
+
+  // 상태
+  // - Disabled
+  // - Loading -> 보류!
+
+  // 버튼 타입
+  // - filled(default)
+  // - outlined
+
+  // Corner Radius
+  // - 기본 Box (r6)
+  // - Round Box (높이 50%)
+
+  enum ButtonType {
+    case filled
+    case outlined
+  }
+
+  enum ButtonSize {
+    case small
+    case medium
+    case large
+    case xLarge
+
+    var height: CGFloat {
+      switch self {
+      case .small:
+        return 32
+      case .medium:
+        return 48
+      case .large:
+        return 55
+      case .xLarge:
+        return 60
+      }
+    }
+
+    var font: Font {
+      switch self {
+      case .small:
+        return .system(size: 11, weight: .medium)
+      case .medium:
+        return .system(size: 13, weight: .semibold)
+      case .large:
+        return .system(size: 16, weight: .semibold)
+      case .xLarge:
+        return .system(size: 20, weight: .bold)
+      }
+    }
+  }
+
+  enum ButtonRadius {
+    case r6
+    case half
+  }
+
+  @Environment(\.isEnabled) private var isEnabled: Bool
+
+  /// 버튼 텍스트 색상
+  private var foregroundColor: Color {
+    if isEnabled {
+      if buttonType == .filled {
+        return UIAsset.black.swiftUIColor
+      } else {
+        return UIAsset.green1.swiftUIColor
+      }
+    } else {
+      if buttonType == .filled {
+        return UIAsset.gray3.swiftUIColor
+      } else {
+        return UIAsset.gray2.swiftUIColor
+      }
+    }
+  }
+
+  /// 버튼 배경색 색상
+  private var backgroundColor: Color {
+    if isEnabled {
+      return UIAsset.green1.swiftUIColor
+    } else {
+      return UIAsset.gray1.swiftUIColor
+    }
+  }
+
+  /// 버튼 텍스트
+  private let title: String
+
+  /// 버튼 타입 (filled[default], outlined)
+  private var buttonType: ButtonType
+
+  /// 버튼 텍스트 (small, medium, large[default], xLarge)
+  private var buttonSize: ButtonSize
+
+  /// 버튼 CornerRadius (r6, half(높이의 절반))
+  private var buttonRadius: ButtonRadius
+
+  /// 버튼 액션
+  private var action: () -> Void
+
+  init(
+    title: String,
+    buttonType: ButtonType = .filled,
+    buttonSize: ButtonSize = .xLarge,
+    buttonRadius: ButtonRadius = .half,
+    action: @escaping () -> Void
+  ) {
+    self.title = title
+    self.buttonType = buttonType
+    self.buttonSize = buttonSize
+    self.buttonRadius = buttonRadius
+    self.action = action
+  }
+
+  var body: some View {
+    Button {
+      self.action()
+    } label: {
+      Text(title)
+        .foregroundColor(foregroundColor)
+        .font(buttonSize.font)
+        .frame(maxWidth: .infinity)
+        .frame(height: buttonSize.height)
+        .background {
+          if buttonType == .filled {
+            RoundedRectangle(cornerRadius: setRadius())
+              .fill(backgroundColor)
+          } else {
+            RoundedRectangle(cornerRadius: setRadius())
+              .stroke(lineWidth: 2)
+              .fill(backgroundColor)
+          }
+        }
+    }
+    .disabled(!isEnabled)
+  }
+
+  private func setRadius() -> CGFloat {
+    switch buttonRadius {
+    case .r6:
+      return 6
+
+    case .half:
+      return buttonSize.height / 2
+    }
+  }
+}
+
+struct SMBoxButton_Previews: PreviewProvider {
+  static var previews: some View {
+    Group {
+      SMBoxButton(title: "확인") {
+        print("확인 클릭")
+      }
+      .environment(\.isEnabled, true)
+      .padding()
+      .preferredColorScheme(.dark)
+      .previewDisplayName("Filled Button")
+      .previewLayout(.sizeThatFits)
+
+      SMBoxButton(title: "확인", buttonRadius: .r6) {
+        print("확인 클릭")
+      }
+      .environment(\.isEnabled, true)
+      .padding()
+      .preferredColorScheme(.dark)
+      .previewDisplayName("Filled Button Radius 6")
+      .previewLayout(.sizeThatFits)
+
+      SMBoxButton(title: "확인") {
+        print("확인 클릭")
+      }
+      .environment(\.isEnabled, false)
+      .padding()
+      .preferredColorScheme(.dark)
+      .previewDisplayName("Filled Button: Disabled")
+      .previewLayout(.sizeThatFits)
+
+      SMBoxButton(title: "확인", buttonType: .outlined) {
+        print("확인 클릭")
+      }
+      .environment(\.isEnabled, true)
+      .padding()
+      .preferredColorScheme(.dark)
+      .previewDisplayName("Outlined Button")
+      .previewLayout(.sizeThatFits)
+
+      SMBoxButton(title: "확인", buttonType: .outlined) {
+        print("확인 클릭")
+      }
+      .environment(\.isEnabled, false)
+      .padding()
+      .preferredColorScheme(.dark)
+      .previewDisplayName("Outlined Button: Disabled")
+      .previewLayout(.sizeThatFits)
+    }
+  }
+}
