d84d2a8fbafb94294e5e9e37573a168bfa82bcd0
dudu <whqtkf12@naver.com>
2023-09-18 16:36:59 +0900

    refactor: LoginView 구현중

--- Projects/LoginFeature/Sources/Login/LoginView.swift
+++ Projects/LoginFeature/Sources/Login/LoginView.swift
@@ -14,33 +14,57 @@ public struct LoginView: View {
   }
   
   public var body: some View {
-    VStack(spacing: 12) {
+    VStack {
       Spacer()
       
-      Button {
-        
-      } label: {
-        Text("카카오톡 으로 시작하기")
-          .foregroundColor(.ds(.black))
-          .padding(.vertical, 20)
-          .frame(maxWidth: .infinity)
-          .background(Color.ds(.yellow))
-          .clipShape(Capsule())
-      }
-
-      Button {
-        
-      } label: {
-        Text("Apple로 시작하기")
-          .foregroundColor(.ds(.black))
-          .padding(.vertical, 20)
-          .frame(maxWidth: .infinity)
-          .background(Color.ds(.white))
-          .clipShape(Capsule())
+      VStack(spacing: 12) {
+        KakaoButton
+        AppleButton
       }
     }
-    .debug()
     .padding(.horizontal, 20)
+  }
+}
+
+private extension LoginView {
+  var KakaoButton: some View {
+    Button {
+      
+    } label: {
+      Text("카카오톡 으로 시작하기")
+        .foregroundColor(.ds(.black))
+        .padding(.vertical, 20)
+        .frame(maxWidth: .infinity)
+        .frame(height: 60)
+        .background(Color.ds(.yellow))
+        .clipShape(Capsule())
+    }
+  }
+  
+  var AppleButton: some View {
+    SignInWithAppleButton(
+      .continue,
+      onRequest: { request in
+        request.requestedScopes = [.fullName, .email]
+      },
+      onCompletion: { result in
+        switch result {
+        case let .success(auth):
+          switch auth.credential {
+          case let appIDCredential as ASAuthorizationAppleIDCredential:
+            let id = appIDCredential.user
+            print(id)
+          default:
+            break
+          }
+        case let .failure(error):
+          print(error.localizedDescription)
+        }
+      }
+    )
+    .signInWithAppleButtonStyle(.white)
+    .clipShape(Capsule())
+    .frame(height: 60)
   }
 }
 
