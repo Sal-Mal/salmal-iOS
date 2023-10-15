da7047072cdcd378b3a0b9d66987b51ed77c2bb0
devseu <seu0313@kakao.com>
2023-10-15 13:23:19 +0900

    feat: SMImagePicker 추가

---
+++ Projects/UI/Sources/Components/ImagePicker/SMImagePicker.swift
@@ -0,0 +1,57 @@
+import SwiftUI
+
+public struct SMImagePicker: UIViewControllerRepresentable {
+
+  public typealias UIViewControllerType = UIImagePickerController
+
+  private let onCapture: (Data?) -> Void
+  private let onDismiss: () -> Void
+
+  public init(onCapture: @escaping (Data?) -> Void, onDismiss: @escaping () -> Void) {
+    self.onCapture = onCapture
+    self.onDismiss = onDismiss
+  }
+
+  public func makeUIViewController(context: Context) -> UIImagePickerController {
+    let imagePicker = UIImagePickerController()
+    imagePicker.allowsEditing = false
+    imagePicker.sourceType = .camera
+    imagePicker.delegate = context.coordinator
+
+    return imagePicker
+  }
+
+  public func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) { }
+
+  public func makeCoordinator() -> Coordinator {
+    Coordinator(delegate: self)
+  }
+
+
+  // MARK: - Coordinator (Delegate)
+
+  public final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    private let delegate: SMImagePicker
+
+    init(delegate: SMImagePicker) {
+      self.delegate = delegate
+    }
+
+    public func imagePickerController(
+      _ picker: UIImagePickerController,
+      didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]
+    ) {
+      guard let image = info[.originalImage] as? UIImage else {
+        delegate.onDismiss()
+        return
+      }
+
+      delegate.onCapture(image.jpegData(compressionQuality: 0.4))
+    }
+
+    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+      delegate.onDismiss()
+    }
+  }
+}
