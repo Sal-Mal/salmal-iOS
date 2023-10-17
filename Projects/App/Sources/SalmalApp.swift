8b85ba360a5a4463651fba9fcc739d4cc77d7128
dudu <whqtkf12@naver.com>
2023-10-17 10:43:13 +0900

    feat: Default TabBar 구현

--- Projects/App/Sources/SalmalApp.swift
+++ Projects/App/Sources/SalmalApp.swift
@@ -2,12 +2,15 @@ import SwiftUI
 
 import UI
 import Core
-import MainFeature
+
 import LoginFeature
+import MainFeature
+import ProfileFeature
 
 @main
 struct SalmalApp: App {
   @State private var isLogined = false
+  @State private var tabIndex = 0
   
   init() {
     SM.Font.initFonts()
@@ -17,9 +20,34 @@ struct SalmalApp: App {
     WindowGroup {
       Group {
         if isLogined {
-          SalMalView(store: .init(initialState: .init()) {
-            SalMalCore()
-          })
+          
+          TabView(selection: $tabIndex) {
+            SalMalView(store: .init(initialState: .init()) {
+              SalMalCore()
+            })
+            .tabItem {
+              Image(icon: tabIndex == 0 ? .home_fill : .home)
+                .fit(size: 32)
+            }
+            .tag(0)
+            
+            Rectangle()
+            .tabItem {
+              Image(icon: tabIndex == 1 ? .plus_circle : .plus)
+                .fit(size: 32)
+            }
+            .tag(1)
+            
+            ProfileView(store: .init(initialState: .init()) {
+              ProfileCore()
+            })
+            .tabItem {
+              Image(icon: tabIndex == 2 ? .person_fill : .person)
+                .fit(size: 32)
+            }
+            .tag(2)
+          }
+          
         } else {
           SplashView(store: .init(initialState: .init()) {
             SplashCore()
