8285466f13a8c6cab5a3ebbd4bda699f06342a8b
dudu <whqtkf12@naver.com>
2024-02-29 08:40:42 +0900

    feat: 게시글 신고시 신고하기 이유 추가

--- Projects/ProfileFeature/Sources/Views/Report/ReportCore.swift
+++ Projects/ProfileFeature/Sources/Views/Report/ReportCore.swift
@@ -5,6 +5,8 @@ import ComposableArchitecture
 public struct ReportCore: Reducer {
   public struct State: Equatable {
     @PresentationState var alert: AlertState<Action.Alert>?
+    @PresentationState var confirmDialog: ConfirmationDialogState<Action.ConfirmationDialog>?
+    
     let voteID: Int
     let memberID: Int
     let items: [MenuItem] = [
@@ -19,12 +21,22 @@ public struct ReportCore: Reducer {
   }
   
   public enum Action: Equatable {
-    case alert(PresentationAction<Alert>)
-    case setAlert(String)
+    case _alert(PresentationAction<Alert>)
+    case _confirmDialog(PresentationAction<ConfirmationDialog>)
+    case _setAlert(String)
+    
     case tap(index: Int)
     
-    public enum Alert {
-      case tapConfirmButton
+    public enum Alert: Equatable {
+      case confirm
+    }
+    
+    public enum ConfirmationDialog: Equatable {
+      case badWord
+      case copyright
+      case pornography
+      case advertisement
+      case etc
     }
   }
   
@@ -40,50 +52,91 @@ public struct ReportCore: Reducer {
   public var body: some ReducerOf<Self> {
     Reduce { state, action in
       switch action {
-      case .alert(.presented(.tapConfirmButton)):
+      case ._alert(.presented(.confirm)):
         return .run { send in
           await dismiss()
         }
       
-      case .alert:
-        return .none
+      case ._alert: break
         
-      case let .setAlert(message):
+      case ._confirmDialog(.presented):
+          return .run { [id = state.voteID] send in
+            try await voteRepository.report(voteID: id)
+            await send(._setAlert("신고 처리가 완료되었습니다.\n24시간 이내에 검토 후 삭제 조치될 예정입니다."))
+          } catch: { error, send in
+            if let error = error as? SMError,
+               case let .network(.invalidURLHTTPResponse(code)) = error,
+               code == 3006
+            {
+              await send(._setAlert("이미 신고한 게시글입니다"))
+            } else {
+              await toastManager.showToast(.error("게시물 신고 실패"))
+              await dismiss()
+            }
+          }
+        
+      case ._confirmDialog:
+        break
+        
+      case let ._setAlert(message):
         state.alert = AlertState {
           TextState("")
         } actions: {
-          ButtonState(action: .tapConfirmButton) {
+          ButtonState(action: .confirm) {
             TextState("확인")
           }
         } message: {
           TextState(message)
         }
         
-        return .none
       case let .tap(index):
         switch index {
         case 0:
-          return .run { [id = state.voteID] send in
-            try await voteRepository.report(voteID: id)
-            await send(.setAlert("신고 처리가 완료되었습니다.\n24시간 이내에 검토 후 삭제 조치될 예정입니다."))
-          } catch: { error, send in
-            await toastManager.showToast(.error("게시물 신고 실패"))
-            await dismiss()
+          state.confirmDialog = ConfirmationDialogState {
+            TextState("")
+          } actions: {
+            ButtonState(role: .cancel) {
+              TextState("취소")
+            }
+            
+            ButtonState(action: .badWord) {
+              TextState("욕설/인신공격/비방")
+            }
+            
+            ButtonState(action: .copyright) {
+              TextState("개인정보/저작권 침해")
+            }
+            
+            ButtonState(action: .pornography) {
+              TextState("음란성/선정성 게시물")
+            }
+            
+            ButtonState(action: .advertisement) {
+              TextState("영리목적/홍보성 게시물")
+            }
+            
+            ButtonState(action: .etc) {
+              TextState("기타")
+            }
+          } message: {
+            TextState("신고 이유")
           }
         case 1:
           return .run { [id = state.memberID] send in
             try await memberRepository.block(id: id)
-            await send(.setAlert("해당 유저를 차단했어요.\n더 이상 해당 유저의 게시글이 피드에 보이지 않아요"))
+            await send(._setAlert("해당 유저를 차단했어요.\n더 이상 해당 유저의 게시글이 피드에 보이지 않아요"))
           } catch: { error, send in
             await toastManager.showToast(.error("유저 차단 실패"))
             await dismiss()
           }
           
-        default:
-          return .none
+        default: break
         }
       }
+      
+      return .none
     }
-    .ifLet(\.$alert, action: /Action.alert)
+    .ifLet(\.$alert, action: /Action._alert)
+    .ifLet(\.$confirmDialog, action: /Action._confirmDialog)
   }
 }
--- Projects/ProfileFeature/Sources/Views/Report/ReportView.swift
+++ Projects/ProfileFeature/Sources/Views/Report/ReportView.swift
@@ -26,7 +26,8 @@ public struct ReportView: View {
           }
       }
     }
-    .alert(store: store.scope(state: \.$alert, action: { .alert($0) }))
+    .alert(store: store.scope(state: \.$alert, action: { ._alert($0) }))
+    .confirmationDialog(store: store.scope(state: \.$confirmDialog, action: { ._confirmDialog($0)}))
     .onAppear {
       BlurManager.shared.blurBackground()
     }
